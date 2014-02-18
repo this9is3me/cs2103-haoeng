5148f467ee32a2e53fed0ab4795d234d29fdb215
Hao Eng <this9is3me@gmail.com>
2014-02-19 01:07:13 +0800

    Correction for CE1

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -6,68 +5,172 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Vector;
 
 /**
- * @author Hao Eng
- *
+ * @author Hao Eng Assumption: the file will be saved after every operation.
  */
-//assumption: the file will be saved after every operation
+
 public class TextBuddy {
+	// for numbering in the list.
+	public static int count = 0;
+	public static String WELCOME_MSG = "Welcome to TextBuddy. %s is ready for use.\n",
+			COMMAND = "command: ",
+			ADD = "add",
+			DISPLAY = "display",
+			CLEAR = "clear", DELETE = "delete", EXIT = "exit";
+	public static Vector<String> list = new Vector<String>();
+	public static Scanner sc = new Scanner(System.in);
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	static int count = 0; //for numbering in the list
-
 	public static void main(String[] args) throws Exception {
-		String filename = null;
-		Vector<String> list = new Vector<String>();
-		Scanner sc = new Scanner(System.in);
-		//taking in the filename
-		if(args.length > 0){
-			filename  = args[0];
-		}
+		String filename = retrieveFileName(args);
 		copyOldfileToList(filename, list);
-		String command = null;
-		System.out.println("Welcome to TextBuddy. " + filename + " is ready for use");
-		System.out.print("command: ");
-		
-		do {
-			command = sc.nextLine();
-			//checking the action for the command
-			if(command.contains("add")) {
-				String text = command.replace("add ", "");
-				list.add(text);
-				writingFile(text, list.size() , filename);
-				System.out.println("added to " + filename + ": " + "\"" + text + "\"");
-			}
-			
-			if(command.equalsIgnoreCase("display")){
-				printingFile(filename);
-			}
-			
-			if(command.equalsIgnoreCase("clear")){
-				clearFile(filename);
-				list.clear();
-				System.out.println("all content deleted from " + filename);
-			}
-			
-			if(command.contains("delete")){
-				String text = command.replace("delete ", "");
-				removeSelectedLine(text, list, filename);
-			}
-			
-			if(!command.equalsIgnoreCase("exit")){
-				System.out.print("command: ");
-			}
+		printMsg(filename);
+		commandHandler(filename);
+	}
 
-			if(command.equalsIgnoreCase("exit")){
-				sc.close();
-			}
-			
-		} while(!command.equalsIgnoreCase("exit"));	//not an exit command	
+	/**
+	 * @param filename
+	 * @throws FileNotFoundException
+	 */
+	private static void commandHandler(String filename)
+			throws FileNotFoundException {
+		String command = null;
+		do {
+			command = readCommand();
+			checkWordAssignAction(filename, command);
+
+		} while (!keyword(command, EXIT)); // not an exit command
+	}
+
+	/**
+	 * @param filename
+	 * @param command
+	 * @throws FileNotFoundException
+	 */
+	private static void checkWordAssignAction(String filename, String command)
+			throws FileNotFoundException {
+		// checking the action for the command
+		if (keyword(command, ADD)) {
+			String text = replacingAdd(command);
+			list.add(text);
+			writeTextToFile(text, list.size(), filename);
+			printSuccessfulAddMsg(filename, text);
+		}
+
+		if (keyword(command, DISPLAY)) {
+			printContentsOfFile(filename);
+		}
+
+		if (keyword(command, CLEAR)) {
+			clearFile(filename);
+			list.clear();
+			printSuccessfulDeleteMsg(filename);
+		}
+
+		if (keyword(command, DELETE)) {
+			String lineNum = replacingDelete(command);
+			removeSelectedLine(lineNum, list, filename);
+		}
+
+		if (!keyword(command, EXIT)) {
+			printCommandLine();
+		}
+
+		if (keyword(command, EXIT)) {
+			closeFile();
+		}
+	}
+
+	/**
+	 * 
+	 * @param command
+	 * @param word
+	 * @return
+	 */
+	private static boolean keyword(String command, String word) {
+		if (command.startsWith(word)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * @return
+	 */
+	private static String readCommand() {
+		String command;
+		command = sc.nextLine();
+		return command;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private static String replacingDelete(String command) {
+		String text = command.replace("delete ", "");
+		return text;
+	}
+
+	private static void closeFile() {
+		sc.close();
+	}
+
+	private static void printCommandLine() {
+		System.out.print("command: ");
+	}
+
+	/**
+	 * @param filename
+	 */
+	private static void printSuccessfulDeleteMsg(String filename) {
+		System.out.println("all content deleted from " + filename);
+	}
+
+	/**
+	 * @param filename
+	 * @param text
+	 */
+	private static void printSuccessfulAddMsg(String filename, String text) {
+		System.out.println("added to " + filename + ": " + "\"" + text + "\"");
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private static String replacingAdd(String command) {
+		String text = command.replace("add ", "");
+		return text;
+	}
+
+	/**
+	 * @param args
+	 * @return
+	 */
+	private static String retrieveFileName(String[] args) {
+		String filename = null;
+		// taking in the filename
+		if (args.length > 0) {
+			filename = args[0];
+		}
+		return filename;
+	}
+
+	/**
+	 * @param filename
+	 */
+	private static void printMsg(String filename) {
+		System.out.printf(WELCOME_MSG, filename);
+		System.out.print(COMMAND);
 	}
 
 	/**
@@ -79,109 +182,107 @@ public class TextBuddy {
 	private static void copyOldfileToList(String filename, Vector<String> list)
 			throws FileNotFoundException, IOException {
 		File file = new File(filename);
-		//if the file contains previous content
-		if(file.length()!=0){
-			//copy file to the vector
+		// if the file contains previous content
+		if (file.length() != 0) {
+			// copy file to the vector
 			String line = null;
 			// FileReader reads text files in the default encoding.
-	        FileReader fileReader = 
-	            new FileReader(file);
+			FileReader fileReader = new FileReader(file);
 
-	        // Always wrap FileReader in BufferedReader.
-	        BufferedReader bufferedReader = 
-	            new BufferedReader(fileReader);
-	        while((line = bufferedReader.readLine()) != null) {
-	        	//remove the number in front of the sentence
-	        	line = line.replaceAll("[0-9]+. ", "");
-	        	list.add(line);
-	        	count++;
-	        }
-	        bufferedReader.close();
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while ((line = bufferedReader.readLine()) != null) {
+				// remove the number in front of the sentence
+				line = line.replaceAll("[0-9]+. ", "");
+				list.add(line);
+				count++;
+			}
+			bufferedReader.close();
 		}
 	}
-	
-	public static void printingFile(String file){
 
-        // This will reference one line at a time
-        String line = null;
-        File content = new File(file);
-        if(content.length() == 0) System.out.println(file + " is empty");
-        
-        try {
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader = 
-                new FileReader(file);
+	public static void printContentsOfFile(String file) {
 
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader = 
-                new BufferedReader(fileReader);
-            while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            }
-           
-            // Always close files.
-            bufferedReader.close();			
-        }
-        catch(IOException ex) {
-            System.out.println("Error reading file '" + file + "'");	
-        }
+		// This will reference one line at a time
+		String line = null;
+		File content = new File(file);
+		if (content.length() == 0) {
+			System.out.println(file + " is empty");
+		}
+
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(file);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while ((line = bufferedReader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			// Always close files.
+			bufferedReader.close();
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + file + "'");
+		}
 	}
-	
+
 	public static void clearFile(String filename) throws FileNotFoundException {
 		try {
 			File file = new File(filename);
-			FileWriter fw = new FileWriter(file);	
+			FileWriter fw = new FileWriter(file);
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.write("");
 			bw.close();
-			
+
 		}
-		
+
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void writingFile(String text, int index, String filename) {
+
+	public static void writeTextToFile(String text, int index, String filename) {
 		try {
 			File file = new File(filename);
-			FileWriter fw = new FileWriter(file, true);	
+			FileWriter fw = new FileWriter(file, true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.write(index + ". " + text + "\n");
 			bw.close();
-			
+
 		}
-		
+
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private static void removeSelectedLine(String replace, Vector<String> list, String filename) throws FileNotFoundException {
+
+	private static void removeSelectedLine(String lineNum, Vector<String> list,
+			String filename) throws FileNotFoundException {
 		Integer position;
 		try {
-			position = Integer.parseInt(replace);
-		} catch (NumberFormatException e){
+			position = Integer.parseInt(lineNum);
+		} catch (NumberFormatException e) {
 			System.out.println("Delete takes a number argument.");
 			return;
 		}
-		//if the number to delete is out of the list
-		if(position > list.size() || position <= 0){
+		// if the number to delete is out of the list
+		if (position > list.size() || position <= 0) {
 			System.out.println("Invalid deletion");
 			return;
 		}
 
-		System.out.println("deleted from " + filename + ": \"" + list.get(position-1) + "\"");
-		
-		//rewriting files
-		list.removeElementAt(position-1);
+		System.out.println("deleted from " + filename + ": \""
+				+ list.get(position - 1) + "\"");
+
+		// rewriting files
+		list.removeElementAt(position - 1);
 		clearFile(filename);
 		Iterator<String> itr = list.iterator();
 		int count = 0;
-		while(itr.hasNext()){
+		while (itr.hasNext()) {
 			count++;
-			writingFile(itr.next(), count, filename);
+			writeTextToFile(itr.next(), count, filename);
 		}
-	} 
+	}
 }
-
