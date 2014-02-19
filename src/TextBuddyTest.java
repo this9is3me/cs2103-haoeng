6231bdaca7171c7d715178959c3d1efe3a700cc4
this9is3me <this9is3me@localhost>
2014-02-20 00:07:09 +0800

    Test cases

---
+++ src/TextBuddyTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Hao Eng
+ * 
+ */
+public class TextBuddyTest {
+	String filename = "output.txt";
+
+	/**
+	 * Test method for {@link TextBuddy#clearFile(java.lang.String)}.
+	 */
+	@Test
+	public void testClearFile() {
+
+	}
+
+	/**
+	 * Test method for {@link TextBuddy#getCount()}.
+	 */
+	@Test
+	public void testGetCount() {
+		assertEquals(0, TextBuddy.getCount());
+	}
+
+	/**
+	 * Test method for {@link TextBuddy#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+	}
+}
